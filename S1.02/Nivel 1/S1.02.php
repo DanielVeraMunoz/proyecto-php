f92358425f171a2ee121205d6ddb79fae350e234
Danielo <dani.vmg91@gmail.com>
2025-11-26 11:20:35 +0100

    Nivel 1 - Exercise 1 finished.

---
+++ S1.02/Nivel 1/S1.02.php
@@ -0,0 +1,17 @@
+<?php
+
+/* EXERCICI 1 */
+
+$my_integer = 17;
+$my_double = 0.3;
+$my_string = "Pichi";
+$my_boolean = true;
+
+echo "Integer: " . $my_integer . "\n";
+echo "Double: " . $my_double . "\n";
+echo "String: " . $my_string . "\n";
+echo "Boolean: " . $my_boolean . "\n";
+
+define("MY_CONSTANT", "Daniel Vera");
+
+echo "String constant: " . MY_CONSTANT;
